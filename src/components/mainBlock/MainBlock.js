66ea539d93fe3e573457ba5c905ab0bfd24a617a
SkirnevskyiAleksandr <sanya-sha2016@ukr.net>
2022-01-30 22:06:04 +0200

    feat:befre innertext install

--- src/components/mainBlock/MainBlock.js
+++ src/components/mainBlock/MainBlock.js
@@ -5,18 +5,19 @@ import Airbus from "../../assets/Airbus.svg";
 import Forbes from "../../assets/Forbes.svg";
 import Nascar from "../../assets/Nascar.svg";
 import Usc from "../../assets/USC.svg";
-
 import './mainBlock.scss';
 
+
 function MainBlock() {
+
+
     return (
         <section className='mainBlock'>
-
             {/* left-block */}
             <div className='left-block'>
                 <h1>Provide excellent <a href='#'>customer</a>  service.</h1>
-                <p>Answer more tickets with all-in-one help desk software.</p>
-                <p className="left-block_list"> ✓ No setup fee   ✓ Customer service 24/7
+                <p id="answer">Answer more tickets with <br />all-in-one help desk software.</p>
+                <p id="no-step" className="left-block_list"> ✓ No setup fee   ✓ Customer service 24/7<br />
                     ✓ No credit card required   ✓ Cancel any time
                 </p>
                 <div className="left-block_get-started">
@@ -39,6 +40,10 @@ function MainBlock() {
                 </div>
             </div>
         </section>
+
     );
 }
+
+
+
 export default MainBlock
